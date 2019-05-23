1ba3a4b241ea6015d08c268e4efd3a3703771473
Jinyi <jinyi.chen@mail.utoronto.ca>
2019-05-23 13:19:39 -0400

    pratice

---
+++ HelloWorld/hello.java
@@ -0,0 +1,26 @@
+public class hello{
+    public static void main(String[]args){
+        System.out.print("Hello World!");
+
+        int maxValue = 2_123_234;
+        System.out.println(maxValue);
+        
+        int intF = 5 / 2;
+        float floatF = 5f / 3f;
+        double doubleF = 5d / 3d;
+
+        System.out.println("my int value: " + intF);
+        System.out.println("my float value: " + floatF);
+        System.out.println("my double value: " + doubleF);
+
+        char myChar= '\u00A9';
+
+        System.out.println("my char: " + myChar);
+
+        boolean myBoolean = false;
+        
+
+    }
+
+
+}
